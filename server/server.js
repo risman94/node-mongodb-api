ddc2955bf4793cefab014eaa9338bc292d3178ac
risman94 <kamarulrisman1994@gmail.com>
2017-08-30 07:18:50 +0800

    add function delete

--- server/server.js
+++ server/server.js
@@ -45,6 +45,21 @@ app.get('/todos/:id', (req, res) => {
 	}).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectId.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => res.status(400).send());
+});
+
 app.listen(port, () => {
 	console.log('Starting up at port ' + port);
 });
