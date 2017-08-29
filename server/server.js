34555ffc66c195cf4990082e1932b40619cafb92
risman94 <kamarulrisman1994@gmail.com>
2017-08-30 02:11:09 +0800

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+//var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,16 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	Todo.findById(id).then((todo) => {
+		if (todo) {
+			res.status(200).send({todo});
+		}
+		res.status(404).send({'message': 'id not found'});
+	}).catch((e) => res.status(400).send({message: 'id not valid'}));
 });
 
 app.listen(3000, () => {
