7efa66c52e231f9db5de84652dc02d02ab3fa2ac
risman94 <kamarulrisman1994@gmail.com>
2017-08-31 06:03:39 +0800

    hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decode = jwt.verify(token, '123abc');
-console.log('decode', decode);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
+
+var hashedPassword = '$2a$10$C3WMsu/bH6C2VraQKyBR9OeShdyFjggmv2tMsaqXjTKUc1w.TDT2G';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decode = jwt.verify(token, '123abc');
+// console.log('decode', decode);
 
 // var message = 'i am user number 3';
 // var hash = SHA256(message).toString();
